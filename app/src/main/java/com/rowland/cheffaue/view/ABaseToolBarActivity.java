bf3c0bad9f6ac4b421367ae0e2768ffee78dc08b
RowlandOti <rowlandotienoo@gmail.com>
2017-10-29 15:39:46 +0300

    feat: use helper abstract activity for toolbar modification

---
+++ app/src/main/java/com/rowland/cheffaue/view/ABaseToolBarActivity.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2015 Oti Rowland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.rowland.cheffaue.view;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.rowland.cheffaue.R;
+
+import butterknife.BindView;
+
+
+/**
+ * Created by Oti Rowland on 12/20/2015.
+ */
+public class ABaseToolBarActivity extends AppCompatActivity {
+
+    // Class Variables
+    private final String LOG_TAG = ABaseToolBarActivity.class.getSimpleName();
+    // ButterKnife injected Views
+    // The inc_toolbar
+    @Nullable
+    @BindView(R.id.toolbar)
+    protected Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    // Derived classes have access to this method
+    protected void setToolbar(boolean showHomeUp, boolean showTitle, int iconResource) {
+        setToolbar(mToolbar, showHomeUp, showTitle, iconResource);
+    }
+
+    // Derived methods have no direct access to this method
+    public void setToolbar(Toolbar mToolbar, boolean isShowHomeUp, boolean isShowTitle, int iconResource) {
+        // Does the inc_toolbar exist?
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+            // Should we set up home-up button navigation?
+            getSupportActionBar().setDisplayHomeAsUpEnabled(isShowHomeUp);
+            // Should we display the title on the inc_toolbar?
+            getSupportActionBar().setDisplayShowTitleEnabled(isShowTitle);
+            // Should we set logo to appear in inc_toolbar?
+            getSupportActionBar().setIcon(iconResource);
+            //this.mToolbar.setLogo(R.drawable.ic_logo_48px);
+        }
+    }
+
+    public void setToolbarTransparent(boolean isToolbarTransparent) {
+        if (isToolbarTransparent) {
+            // Check for minimum api as Lollipop
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                // Set up  the systemUi flags
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                // Set the status bar tobe transparent
+                getWindow().setStatusBarColor(Color.TRANSPARENT);
+            }
+        }
+    }
+}
